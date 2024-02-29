a08a443279cbbbdcfd443a0340f630562a3649d0
iinhle <sawcyzee@gmail.com>
2024-02-28 17:39:40 -0800

    done

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Describes the console."""
+"""Defines the HBnB console."""
 import cmd
 import re
 from shlex import split
@@ -14,24 +14,29 @@ from models.review import Review
 
 
 def parse(arg):
-    cy_brace = re.search(r"\{(.*?)\}", arg)
-    bracket = re.search(r"\[(.*?)\]", arg)
-    if cy_brace is None:
-        if bracket is None:
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
             return [i.strip(",") for i in split(arg)]
         else:
-            lex = split(arg[:bracket.span()[0]])
-            aretl = [i.strip(",") for i in lex]
-            aretl.append(bracket.group())
-            return aretl
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
     else:
-        lex = split(arg[:cy_brace.span()[0]])
-        aretl = [i.strip(",") for i in lex]
-        aretl.append(cy_brace.group())
-        return aretl
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
     prompt = "(hbnb) "
     __classes = {
         "BaseModel",
@@ -48,31 +53,32 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, arg):
-        arg_dict = {
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
             "count": self.do_count,
             "update": self.do_update
         }
-        watch = re.search(r"\.", arg)
-        if watch is not None:
-            arg_c = [arg[:watch.span()[0]], arg[watch.span()[1]:]]
-            watch = re.search(r"\((.*?)\)", arg_c[1])
-            if watch is not None:
-                c_cmd = [arg_c[1][:watch.span()[0]], watch.group()[1:-1]]
-                if c_cmd[0] in arg_dict.keys():
-                    call = "{} {}".format(arg_c[0], c_cmd[1])
-                    return arg_dict[c_cmd[0]](call)
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
         print("*** Unknown syntax: {}".format(arg))
         return False
 
     def do_quit(self, arg):
-        """Escape c_cmd to exit the program."""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """Escape the EOF signal to exit the program."""
+        """EOF signal to exit the program."""
         print("")
         return True
 
@@ -80,115 +86,122 @@ class HBNBCommand(cmd.Cmd):
         """Usage: create <class>
         Create a new class instance and print its id.
         """
-        arg_c = parse(arg)
-        if len(arg_c) == 0:
-            print("** this class name is missing **")
-        elif arg_c[0] not in HBNBCommand.__classes:
-            print("** this class doesn't exist **")
+        argl = parse(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
         else:
-            print(eval(arg_c[0])().id)
+            print(eval(argl[0])().id)
             storage.save()
 
     def do_show(self, arg):
         """Usage: show <class> <id> or <class>.show(<id>)
         Display the string representation of a class instance of a given id.
         """
-        arg_c = parse(arg)
-        obj_dict = storage.all()
-        if len(arg_c) == 0:
-            print("** this class name is missing **")
-        elif arg_c[0] not in HBNBCommand.__classes:
-            print("** this class doesn't exist **")
-        elif len(arg_c) == 1:
-            print("** id is missing **")
-        elif "{}.{}".format(arg_c[0], arg_c[1]) not in obj_dict:
-            print("** instance is not found **")
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+            print("** no instance found **")
         else:
-            print(obj_dict["{}.{}".format(arg_c[0], arg_c[1])])
+            print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_destroy(self, arg):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
-        arg_c = parse(arg)
-        obj_dict = storage.all()
-        if len(arg_c) == 0:
-            print("** this class name is missing **")
-        elif arg_c[0] not in HBNBCommand.__classes:
-            print("** this class doesn't exist **")
-        elif len(arg_c) == 1:
-            print("** id is missing **")
-        elif "{}.{}".format(arg_c[0], arg_c[1]) not in obj_dict.keys():
-            print("** instance is not found **")
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
         else:
-            del obj_dict["{}.{}".format(arg_c[0], arg_c[1])]
+            del objdict["{}.{}".format(argl[0], argl[1])]
             storage.save()
 
     def do_all(self, arg):
         """Usage: all or all <class> or <class>.all()
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects."""
-        arg_c = parse(arg)
-        if len(arg_c) > 0 and arg_c[0] not in HBNBCommand.__classes:
-            print("** this class name doesn't exist **")
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
         else:
-            obj_l = []
+            objl = []
             for obj in storage.all().values():
-                if len(arg_c) > 0 and arg_c[0] == obj.__class__.__name__:
-                    obj_l.append(obj.__str__())
-                elif len(arg_c) == 0:
-                    obj_l.append(obj.__str__())
-            print(obj_l)
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
 
     def do_count(self, arg):
-        arg_c = parse(arg)
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
         count = 0
         for obj in storage.all().values():
-            if arg_c[0] == obj.__class__.__name__:
+            if argl[0] == obj.__class__.__name__:
                 count += 1
         print(count)
 
     def do_update(self, arg):
-        arg_c = parse(arg)
-        obj_dict = storage.all()
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = parse(arg)
+        objdict = storage.all()
 
-        if len(arg_c) == 0:
-            print("** this class is name missing **")
+        if len(argl) == 0:
+            print("** class name missing **")
             return False
-        if arg_c[0] not in HBNBCommand.__classes:
-            print("** this name class doesn't exist **")
+        if argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
             return False
-        if len(arg_c) == 1:
-            print("** id is missing **")
+        if len(argl) == 1:
+            print("** instance id missing **")
             return False
-        if "{}.{}".format(arg_c[0], arg_c[1]) not in obj_dict.keys():
-            print("** instance is not found **")
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
             return False
-        if len(arg_c) == 2:
-            print("** attribute name is missing **")
+        if len(argl) == 2:
+            print("** attribute name missing **")
             return False
-        if len(arg_c) == 3:
+        if len(argl) == 3:
             try:
-                type(eval(arg_c[2])) != dict
+                type(eval(argl[2])) != dict
             except NameError:
-                print("** value is missing **")
+                print("** value missing **")
                 return False
 
-        if len(arg_c) == 4:
-            obj = obj_dict["{}.{}".format(arg_c[0], arg_c[1])]
-            if arg_c[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[arg_c[2]])
-                obj.__dict__[arg_c[2]] = valtype(arg_c[3])
+        if len(argl) == 4:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
             else:
-                obj.__dict__[arg_c[2]] = arg_c[3]
-        elif type(eval(arg_c[2])) == dict:
-            obj = obj_dict["{}.{}".format(arg_c[0], arg_c[1])]
-            for x, y in eval(arg_c[2]).items():
-                if (x in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[x]) in {str, int, float}):
-                    valtype = type(obj.__class__.__dict__[x])
-                    obj.__dict__[x] = valtype(y)
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
                 else:
-                    obj.__dict__[x] = y
+                    obj.__dict__[k] = v
         storage.save()
 
 
--- tests/test_engine/file_storage.py
+++ tests/test_engine/file_storage.py
@@ -1,46 +1,168 @@
 #!/usr/bin/python3
-"""Describes the FileStorage class."""
+"""Defines unittests for models/engine/file_storage.py.
+Unittest classes:
+    TestFileStorage_instantiation
+    TestFileStorage_methods
+"""
+import os
 import json
+import models
+import unittest
+from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
-from models.city import City
 from models.place import Place
+from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
 
-class FileStorage:
-    """Shows an abstracted storage engine.
-    Attributes:
-        __file__obj (str): The name of the file to save objects to.
-        __obj (dict): A dictionary of instantiated objects.
-    """
-    __file__obj = "file.json"
-    __obj = {}
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
 
-    def all(self):
-        """Return the dictionary __obj."""
-        return FileStorage.__obj
+    def test_FileStorage_instantiation_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
 
-    def new(self, obj):
-        oj_name = obj.__class__.__name__
-        FileStorage.__obj["{}.{}".format(oj_name, obj.id)] = obj
+    def test_FileStorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
 
-    def save(self):
-        ox_dict = FileStorage.__obj
-        obj_dict = {obj: ox_dict[obj].to_dict() for obj in ox_dict.keys()}
-        with open(FileStorage.__file__obj, "w") as f:
-            json.dump(obj_dict, f)
+    def test_FileStorage_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-    def reload(self):
+    def testFileStorage_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializes(self):
+        self.assertEqual(type(models.storage), FileStorage)
+
+
+class TestFileStorage_methods(unittest.TestCase):
+    """Unittests for testing methods of the FileStorage class."""
+
+    @classmethod
+    def setUp(self):
         try:
-            with open(FileStorage.__file__obj) as f:
-                obj_dict = json.load(f)
-                for o in obj_dict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
 
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_all_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
+
+    def test_new(self):
+        bm = BaseModel()
+        us = User()
+        st = State()
+        pl = Place()
+        cy = City()
+        am = Amenity()
+        rv = Review()
+        models.storage.new(bm)
+        models.storage.new(us)
+        models.storage.new(st)
+        models.storage.new(pl)
+        models.storage.new(cy)
+        models.storage.new(am)
+        models.storage.new(rv)
+        self.assertIn("BaseModel." + bm.id, models.storage.all().keys())
+        self.assertIn(bm, models.storage.all().values())
+        self.assertIn("User." + us.id, models.storage.all().keys())
+        self.assertIn(us, models.storage.all().values())
+        self.assertIn("State." + st.id, models.storage.all().keys())
+        self.assertIn(st, models.storage.all().values())
+        self.assertIn("Place." + pl.id, models.storage.all().keys())
+        self.assertIn(pl, models.storage.all().values())
+        self.assertIn("City." + cy.id, models.storage.all().keys())
+        self.assertIn(cy, models.storage.all().values())
+        self.assertIn("Amenity." + am.id, models.storage.all().keys())
+        self.assertIn(am, models.storage.all().values())
+        self.assertIn("Review." + rv.id, models.storage.all().keys())
+        self.assertIn(rv, models.storage.all().values())
+
+    def test_new_with_args(self):
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
+
+    def test_save(self):
+        bm = BaseModel()
+        us = User()
+        st = State()
+        pl = Place()
+        cy = City()
+        am = Amenity()
+        rv = Review()
+        models.storage.new(bm)
+        models.storage.new(us)
+        models.storage.new(st)
+        models.storage.new(pl)
+        models.storage.new(cy)
+        models.storage.new(am)
+        models.storage.new(rv)
+        models.storage.save()
+        save_text = ""
+        with open("file.json", "r") as f:
+            save_text = f.read()
+            self.assertIn("BaseModel." + bm.id, save_text)
+            self.assertIn("User." + us.id, save_text)
+            self.assertIn("State." + st.id, save_text)
+            self.assertIn("Place." + pl.id, save_text)
+            self.assertIn("City." + cy.id, save_text)
+            self.assertIn("Amenity." + am.id, save_text)
+            self.assertIn("Review." + rv.id, save_text)
+
+    def test_save_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.save(None)
+
+    def test_reload(self):
+        bm = BaseModel()
+        us = User()
+        st = State()
+        pl = Place()
+        cy = City()
+        am = Amenity()
+        rv = Review()
+        models.storage.new(bm)
+        models.storage.new(us)
+        models.storage.new(st)
+        models.storage.new(pl)
+        models.storage.new(cy)
+        models.storage.new(am)
+        models.storage.new(rv)
+        models.storage.save()
+        models.storage.reload()
+        objs = FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + bm.id, objs)
+        self.assertIn("User." + us.id, objs)
+        self.assertIn("State." + st.id, objs)
+        self.assertIn("Place." + pl.id, objs)
+        self.assertIn("City." + cy.id, objs)
+        self.assertIn("Amenity." + am.id, objs)
+        self.assertIn("Review." + rv.id, objs)
+
+    def test_reload_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.reload(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
